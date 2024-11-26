6081c648c8ae9c46b2149b0ec8ffa5b18ef3d0bf
mino <c0b231317b@edu.teu.ac.jp>
2024-11-26 14:27:33 +0900

    練習２：爆弾描画

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -12,11 +13,20 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    # pr_2
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = 5, 5
+
+    #
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
-    # 練習１
+    # pr_１
     DELTA = {
         pg.K_UP : (0,-5), 
         pg.K_DOWN : (0, 5),
@@ -37,6 +47,8 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
+        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx, vy)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
